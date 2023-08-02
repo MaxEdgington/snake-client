8e38a0528a7102e7fd82d55e12c0bd42d403f89d
MaxEdgington <edgington.m.w@gmail.com>
2023-08-02 15:42:36 -0400

    working on movement for the snek game

--- client.js
+++ client.js
@@ -19,37 +19,35 @@ const net = require("net");
 const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
-    port: "50541"
+    port: "50541",
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on('connect', () => {
+  conn.on("connect", () => {
     console.log("Successfully connected to game");
-    
   });
 
   conn.on("connect", () => {
     conn.write("Name: MAX");
   });
- 
-  conn.on('data', (data) => {
+
+  /*
+  conn.on("connect", () => {
+    conn.write("Move: up");
+  }),
+
+  */
+
+  conn.on("data", (data) => {
     console.log(data.toString());
     conn.end();
   });
 
-
-
-
-
   return conn;
 };
 
-
-
-
-
 module.exports = {
-  connect
+  connect,
 };
