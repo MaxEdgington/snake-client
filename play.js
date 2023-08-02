c9e43104f8f217bb043b1a5bfe3525009c0c453f
MaxEdgington <edgington.m.w@gmail.com>
2023-08-02 16:14:21 -0400

    created an input file to host setupInput & handleUserInput + export / import into play.js

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require("./client.js");
+const { setupInput } = require("./input");
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(data) {
-  
-  if (data === "\u0003") {
-    process.exit();
-  }
-  
-};
 
 console.log("Connecting ...");
 connect();
+setupInput();
